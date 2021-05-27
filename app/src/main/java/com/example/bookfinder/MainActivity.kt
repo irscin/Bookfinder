ba84f0cacea1a33ce53bd634ff3506ac56319ee1
Italo Soares <irs@cin.ufpe.br>
2021-05-27 12:53:10 -0300

    Layout finalizado

--- app/src/main/java/com/example/bookfinder/MainActivity.kt
+++ app/src/main/java/com/example/bookfinder/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.bookfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.bookfinder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        binding.findButton.setOnClickListener{
+            //Write code here
+        }
+
     }
 }
