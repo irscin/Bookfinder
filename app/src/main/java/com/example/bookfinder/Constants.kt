5348724c70ded70063e5531657f00f81c56f3ecd
Italo Soares <irs@cin.ufpe.br>
2021-05-27 15:13:09 -0300

    Working on coroutine for API async call

---
+++ app/src/main/java/com/example/bookfinder/Constants.kt
@@ -0,0 +1,3 @@
+object Constants {
+    const val API_URL = "https://www.googleapis.com/books/v1/volumes?q="
+}
--- app/src/main/java/com/example/bookfinder/MainActivity.kt
+++ app/src/main/java/com/example/bookfinder/MainActivity.kt
@@ -2,18 +2,54 @@ package com.example.bookfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.bookfinder.databinding.ActivityMainBinding
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    private val client = OkHttpClient();
+
+
+    private fun run(url: String): Response {
+        val request = Request.Builder().url(url).build()
+
+        return client.newCall(request).execute()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
 
+
+
         binding.findButton.setOnClickListener{
-            //Write code here
+            //Setting up flags to make shorter code
+            var emptyTitle = false
+            var emptyAuthor = false
+            var emptyPublisher = false
+            var bookTitle = binding.titleEdit.text.toString()
+            var bookAuthor = binding.authorEdit.text.toString()
+            var bookPublisher = binding.titleEdit.text.toString()
+            //Checking what fields are empty
+            if(bookTitle==""){
+                emptyTitle=true
+            }
+            if(bookAuthor==""){
+                emptyAuthor=true
+            }
+            if(bookPublisher==""){
+                emptyPublisher=true
+            }
+            bookTitle=bookTitle.replace(" ","+")
+            Log.d("1",bookTitle)
+            Log.d("2",Constants.API_URL+bookTitle)
+            val t = run("https://www.googleapis.com/books/v1/volumes?q=love+math")
+            Log.d("3",t.toString())
         }
 
     }
